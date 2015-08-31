85a5995d59d2f8fe5ce85d2fbf7fe350b8c79721
zhaogaolei <arnold.zhao@31huiyi.com>
2015-08-31 15:09:37 +0800

    发

---
+++ quartz/IScheduledJob.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace quartz
+{
+    public interface IScheduledJob
+    {
+        void Run();
+    }
+}
---
+++ quartz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace quartz
+{
+    //tutorial url
+    //http://geekswithblogs.net/TarunArora/archive/2012/11/17/quartz.net-writing-your-first-hello-world-job.aspx
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+             // Infinite loop, so that the console doesn't close on you
+                while (true)
+                {
+                    var sj = new ScheduledJob();
+                    sj.Run();
+
+                    Console.WriteLine(@"{0}Check Quartz.net\Trace\application.log.txt for Job updates{0}",
+                                        Environment.NewLine);
+
+                    Console.WriteLine("{0}Press Ctrl^C to close the window. The job will continue " +
+                                        "to run via Quartz.Net windows service, " +
+                                        "see job activity in the Quartz.Net Trace file...{0}",
+                                        Environment.NewLine);
+
+                    Thread.Sleep(10000 * 100000);    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed: {0}", ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
